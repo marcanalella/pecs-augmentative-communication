72e08259bb32959530028b11df2c5ce130f85043
Canalella <mario.canalella-cic [at] ibm [dot] com>
2022-06-11 13:59:16 +0200

    fix website redirect

--- app/lib/constants.dart
+++ app/lib/constants.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
 const SERVER_IP = 'https://pecs-be.herokuapp.com/api/v1';
-const url = "https://www.canalellaauto.it";
+const url = "https://pecs-mobile.netlify.app/";
 
